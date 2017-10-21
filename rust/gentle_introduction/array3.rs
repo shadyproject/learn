78381dc5e1365ccc8ac973a8aa090a6577eb4ab9
Shady <shady@shadyproject.net>
2017-10-21 10:05:58 -0700

    Files

---
+++ rust/gentle_introduction/array3.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let ints = [1, 2, 3];
+    let floats = [1.1, 2.1, 3.1];
+    let strings = ["hello", "world"];
+    let ints_ints = [[1,2], [2,10]];
+
+    println!("Ints: {:?}", ints);
+    println!("Floats: {:?}", floats);
+    println!("Strings: {:?}", strings);
+    println!("Ints Ints: {:?}", ints_ints);
+}
