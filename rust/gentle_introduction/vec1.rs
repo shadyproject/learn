71863a02a4a79f109c1670e2292b747b8c5586fa
Chris Martin <shady@shadyproject.net>
2017-11-05 16:08:00 -0800

    vectors and slices

---
+++ rust/gentle_introduction/vec1.rs
@@ -0,0 +1,14 @@
+// vec1.rs
+fn main() {
+    let mut v = Vec::new();
+    v.push(10);
+    v.push(20);
+    v.push(30);
+
+    let first = v[0]; //will panic if out of range
+    let maybe_first = v.get(0);
+
+    println!("v is {:?}", v);
+    println!("first is {}", first);
+    println!("maybe first is {:?}", maybe_first);
+}
